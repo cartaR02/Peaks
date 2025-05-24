03a891a1dd0602f44b55200e11117d46f9f8258b
Carter Struck <carterstruck02@gmail.com>
2025-05-24 16:28:15 -0400

    added things

---
+++ app/LoggingIn/Login.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import {
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    KeyboardAvoidingView, Alert,
+    View,
+    Image,
+} from 'react-native';
+import GlobalStyles from '../constants/globalStyles';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [Loading, setLoading] = useState(false);
+
+
+
+    return (
+        <SafeAreaView style={[GlobalStyles.background,  styles.container ]}>
+            <View style={styles.formContainer}>
+                <Image
+                    source={require('../../assets/images/MainSplash.png')}
+                    resizeMode="contain"
+                    style={{
+                        marginTop: 10,
+                        width: '80%',
+                        height: '40%',
+                        aspectRatio: 1,
+                        alignSelf: 'center',
+                    }}
+                />
+                <Text style={styles.title}>Peaks</Text>
+
+                <View style={styles.inputWrapper}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Email"
+                        placeholderTextColor="#ccc"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        value={password}
+                        placeholderTextColor="#ccc"
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        autoCapitalize="none"
+                    />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Log In</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Sign Up</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    formContainer: {
+        width: '80%',
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center',
+        marginBottom: 25,
+        marginTop: 25,
+        fontWeight: 'bold',
+        color: '#C800FF',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 15,
+        color: "#fff",
+    },
+    inputWrapper: {
+        padding: 10,
+    },
+    buttonContainer: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+    },
+    button: {
+        backgroundColor: '#C800FF',
+        padding: 15,
+        borderRadius: 5,
+        marginVertical: 10,
+        width: '48%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '600',
+    }
+});
