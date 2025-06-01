2f2f168570a0095c4c91f2590ed2c322b758ecc5
Carter Struck <carterstruck02@gmail.com>
2025-05-31 22:58:36 -0400

    tweaking database storage naming

--- app/Utility/storeWorkout.js
+++ app/Utility/storeWorkout.js
@@ -1,14 +1,27 @@
 // ✅ storeWorkout.js
 
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc,setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../../firebaseConfig'; // make sure this path is correct
+
+const buildWorkoutId = () => {
+  const now = new Date();
+  const mm = String(now.getMonth() + 1).padStart(2, '0');
+  const weekday = now.toLocaleString('default', { weekday: 'long' });
+  const dd = String(now.getDate()).padStart(2, '0');
+  const yyyy = now.getFullYear();
+
+  return `${weekday}-${mm}-${dd}-${yyyy}`;
+}
+
 
 export const StoreWorkout = async (compiledWorkouts) => {
   try {
-    const workoutsRef = collection(db, 'workouts');
+    const workoutId = buildWorkoutId();
+    const workoutsRef  = doc(collection(db, 'CompletedWorkouts'), workoutId);
 
-    await addDoc(workoutsRef, {
-      timestamp: new Date().toISOString(),
+
+    await setDoc(workoutsRef, {
+      timestamp: serverTimestamp(),
       exercises: compiledWorkouts,
     });
 
